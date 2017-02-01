dbf19bd83839a42ed95139ff7cc15aea6449ff61
Matteo Bertini <matteo@naufraghi.net>
2017-02-01 22:49:40 +0100

    ci: install python on osx with a versioned brew tap

--- ci/osx-install.sh
+++ ci/osx-install.sh
@@ -1,4 +1,4 @@
-# osx install step
-git clone https://github.com/MacPython/terryfy
-source terryfy/travis_tools.sh
-get_python_environment $pydist $pyver .venv
+#!/bin/sh
+
+brew tap zoidbergwill/python
+brew install $BREW_PYTHON
